eb35c1d753a2de00e77f650d2f9164d65f10acd0
Ahafonov-mykyta <ahafonovmykyta@gmail.com>
2022-06-30 19:45:02 +0300

    9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const body = document.querySelector ("body");
+const colorName = document.querySelector(".color");
+
+const btn = document.querySelector(".change-color");
+
+btn.addEventListener ("click", (event)=> {
+
+ colorName.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ body.style.backgroundColor = colorName.textContent;
+
+}) 
